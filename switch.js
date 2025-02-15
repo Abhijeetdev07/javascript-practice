36a00da7cc079393d7cd54c339983cffda55fe7e
Abhijeet Ugale <ugaleabhijeet03@gmail.com>
2025-02-15 16:20:03 +0530

    switch-case code

---
+++ switch.js
@@ -0,0 +1,28 @@
+let day = prompt("enter date");
+
+switch(day){
+    case "1" :
+        console.log("a monday");
+        break;
+    case "2" :
+        console.log("e tue");
+        break;
+    case "3" :
+      console.log("wed");
+      break;
+    case "4" :
+        console.log("thr ");
+        break;
+    case "5" :
+      console.log("fri ");
+      break;
+      case "6" :
+        console.log("sat");
+        break;
+    case "7" :
+        console.log("sun ");
+        break;
+    default :
+    alert("please enter a number between 1 to 7");
+
+} 
